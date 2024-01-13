90ecb0162c863c06bd16fa641809b157b8f8bc5b
zyds <13716580192@163.com>
2024-01-13 22:04:53 +0800

    transformers 4.36.2 adaptation

--- 01-Getting Started/01-introduction/demo.py
+++ 01-Getting Started/01-introduction/demo.py
@@ -1,6 +1,7 @@
 # 导入gradio
 import gradio as gr
 # 导入transformers相关包
-from transformers import *
+from transformers import pipeline
 # 通过Interface加载pipeline并启动阅读理解服务
+# 如果无法通过这种方式加载，可以采用离线加载的方式
 gr.Interface.from_pipeline(pipeline("question-answering", model="uer/roberta-base-chinese-extractive-qa")).launch()
